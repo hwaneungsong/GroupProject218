e7b3f89ed0d0860a30a6d41fcfc55beae432a3aa
Philipe <pcq2@njit.edu>
2023-12-18 15:18:40 -0500

    ProductSection

---
+++ hushhaven/app/components/ProductSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./ProductSection.module.css";
+
+export default function ProductSection() {
+  return (
+    <section className={styles.productSection}>
+      <h2 className={styles.productHeader}>Shop Our Products</h2>
+      <div className={styles.productContainer}>
+        <div className={styles.productCard}>
+          <h3 className={styles.productTitle}>TOWELS</h3>
+          <img src="/images/Towels.png" alt="Towels" className={styles.productImage}/>
+        </div>
+        <div className={styles.productCard}>
+          <h3 className={styles.productTitle}>ROBES</h3>
+          <img src="/images/Robes.png" alt="Robes" className={styles.productImage}/>
+        </div>
+      </div>
+    </section>
+  );
+}
